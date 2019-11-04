4710e76a9bbcfabe5f48bcf30dc9270ed84167ea
MATVEN <mattiavena2993@gmail.com>
2019-11-04 15:49:13 +0100

    add timing function

---
+++ sexcolor/js/main.js
@@ -0,0 +1,7 @@
+//Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+//Sulla pagina html deve apparire “Ciao <nome>“,
+//il colore del nome deve essere azzurro o rosa a seconda del sesso inserito.
+
+// variabili
+var name = prompt('Scrivi il tuo nome');
+var sex = prompt('Specifica il tuo sesso');
--- simon/js/main.js
+++ simon/js/main.js
@@ -22,8 +22,18 @@ while (numbArray.length < 5) {
     alert(numbArray);
   }
 
-// l'utente dovrà inserire i 5 numeri casuali
-for (var i = 0; i < 5; i++) {
-  var numero = parseInt(prompt('Inserisci uno dei cinque numeri memorizzati'));
-  console.log(numero);
+  // l'utente dovrà inserire i 5 numeri casuali
+  function numberUser() {
+    for (var i = 0; i < 5; i++) {
+      var numero = parseInt(prompt('Inserisci uno dei cinque numeri memorizzati'));
+      console.log(numero);
+    }
+  }
+
+// imposto il tempo da far trascorrere prima che l'utente possa rispondere
+setTimeout(numberUser, 3000);
+function numberUser() {
+  
 }
+
+// check del software dei numeri inseriti
